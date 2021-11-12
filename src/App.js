c0d6bb91410cad740f81c90a67e789ec839d31f8
Brayan Santos <santos.a.brayan97@gmail.com>
2021-11-11 22:59:37 -0500

    code update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,157 @@
-import logo from './logo.svg';
-import './App.css';
+// Import All Our Components
+import AllPosts from "./pages/AllPosts";
+import SinglePost from "./pages/SinglePost";
+import Form from "./pages/Form";
 
-function App() {
+// Import React and hooks
+import React, { useState, useEffect } from "react";
+
+// Import components from React Router
+import { Route, Switch, Link } from "react-router-dom";
+
+function App(props) {
+  ////////////////////
+  // Style Objects
+  ////////////////////
+
+  const h1 = {
+    textAlign: "center",
+    margin: "10px",
+  };
+
+  const button = {
+  backgroundColor: "navy",
+  display: "block",
+  margin: "auto",
+};
+
+  ///////////////
+  // State & Other Variables
+  ///////////////
+
+  // Our Api Url
+  const url = "https://santos-django-todo-api.herokuapp.com/todos/";
+
+  // State to Hold The List of Posts
+  const [posts, setPosts] = useState([]);
+
+  const nullTodo = {
+    subject: '',
+    details: ''
+  }
+  const [targetTodo, setTargetTodo] = useState(nullTodo)
+
+
+  //////////////
+  // Functions
+  //////////////
+  const getTodos = async () => {
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      setPosts(data.reverse())
+    }catch(e){
+      console.error(e)
+    }
+  }
+
+  const addTodos = async (newTodo) => {
+    try {
+      const response = await fetch(url, {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newTodo),
+    }).then(data => getTodos())
+    }catch(e){
+        console.error(e)
+    }
+  // get updated list of todos
+  }
+
+  const getTargetTodo = (todo) => {
+    setTargetTodo(todo);
+    props.history.push('/edit');
+  }
+
+  const updateTodo = async (todo) => {
+    try {
+      const response = await fetch(url + todo.id + '/', {
+        method: 'put',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(todo)
+      }).then(data => getTodos())
+    }catch(e){
+        console.error(e)
+    }
+  }
+
+  const deleteTodo = async (todo) => {
+    try {
+      const response = await fetch(url + todo.id + "/", {
+        method: "delete",
+      }).then(data => getTodos())
+    } catch (e) {
+      console.error(e)
+    }finally{
+      window.location.assign('/')
+    }
+
+};
+
+  //////////////
+  // useEffects
+  //////////////
+  useEffect(()=>{
+    getTodos();
+  }, [])
+  /////////////////////
+  // returned JSX
+  /////////////////////
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 style={h1}>My Todo List</h1>
+      <Link to="/new">
+        <button style={button}>Create New Todo</button>
+      </Link>
+      <Switch>
+        <Route
+          exact
+          path="/"
+          render={(routerProps) => <AllPosts {...routerProps} posts={posts} />}
+        />
+        <Route
+          path="/post/:id"
+          render={(routerProps) => (
+            <SinglePost {...routerProps} posts={posts}  edit={getTargetTodo} deleteTodo={deleteTodo}/>
+          )}
+        />
+        <Route
+          path="/new"
+          render={(routerProps) =>(
+            <Form
+            {...routerProps}
+            initialTodo={nullTodo}
+            handleSubmit={addTodos}
+            buttonLabel="Create Todo"
+            />
+          )}
+        />
+        <Route
+          path="/edit"
+          render={(routerProps) => (
+            <Form
+            {...routerProps}
+            initialTodo={targetTodo}
+            handleSubmit={updateTodo}
+            buttonLabel="Update Todo"
+            />)
+          }
+        />
+      </Switch>
     </div>
   );
 }
